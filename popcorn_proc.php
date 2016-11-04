1d96aa823c3987903b57952d8d92286739a48324
iShinta <ho_minh_tri@yahoo.fr>
2016-11-04 17:34:16 -0500

    Compute 0.2

--- popcorn_proc.php
+++ popcorn_proc.php
@@ -35,82 +35,32 @@
     $lineNb = (int)(fgets($fh));
     for($i = 0; $i < $lineNb; $i++){
       $productid = "product".$i;
-      echo $productid;
       $productName = (fgets($fh));
-
       $productPrice = (int)(fgets($fh));
-
       $productItem = $_POST[$productid];
+
+      $nbItems += $productItem;
+      $totalProduct = (int)$productPrice*(int)$productItem;
+      $totalBill += $totalProduct;
+
       if ($productItem == "") $productItem = 0;?>
       <tr align = "center">
         <td> <?php print ("$productName"); ?> </td>
         <td> <?php printf ("$ %4.2f", $productPrice); ?> </td>
         <td> <?php print ("$productItem"); ?> </td>
-        <td> <?php printf ("$ %4.2f", (int)$productPrice*(int)$productItem); ?>
+        <td> <?php printf ("$ %4.2f", $totalProduct); ?>
         </td>
       </tr>
       <?php
     }
     fclose($fh);
-
-    // Get form data values
-    //TODO: Replace with array of items. From Form or from File?
-    $unpop = $_POST["unpop"];
-    $caramel = $_POST["caramel"];
-    $caramelnut = $_POST["caramelnut"];
-    $toffeynut = $_POST["toffeynut"];
-
-    //TODO: See previous. If any of the quantities are blank, set them to zero
-    if ($unpop == "") $unpop = 0;
-    if ($caramel == "") $caramel = 0;
-    if ($caramelnut == "") $caramelnut = 0;
-    if ($toffeynut == "") $toffeynut = 0;
-
-
-
-    //TODO: See previous. Compute the item costs and total cost
-    $unpop_cost = 3.0 * $unpop;
-    $caramel_cost = 3.5 * $caramel;
-    $caramelnut_cost = 4.5 * $caramelnut;
-    $toffeynut_cost = 5.0 * $toffeynut;
-    $total_price = $unpop_cost + $caramel_cost +
-                   $caramelnut_cost + $toffeynut_cost;
-    $total_items = $unpop + $caramel + $caramelnut + $toffeynut;
   ?>
-    <tr align = "center">
-      <td> Unpopped Popcorn </td>
-      <td> $3.00 </td>
-      <td> <?php print ("$unpop"); ?> </td>
-      <td> <?php printf ("$ %4.2f", $unpop_cost); ?>
-      </td>
-    </tr>
-    <tr align = "center">
-      <td> Caramel Popcorn </td>
-      <td> $3.50 </td>
-      <td> <?php print ("$caramel"); ?> </td>
-      <td> <?php printf ("$ %4.2f", $caramel_cost); ?>
-      </td>
-      </tr>
-    <tr align = "center">
-      <td> Caramel Nut Popcorn </td>
-      <td> $4.50 </td>
-      <td> <?php print ("$caramelnut"); ?> </td>
-      <td> <?php printf ("$ %4.2f", $caramelnut_cost); ?>
-      </td>
-    </tr>
-    <tr align = "center">
-      <td> Toffey Nut Popcorn </td>
-      <td> $5.00 </td>
-      <td> <?php print ("$toffeynut"); ?> </td>
-      <td> <?php printf ("$ %4.2f", $toffeynut_cost); ?>
-      </td>
-    </tr>
   </table>
   <p /> <p />
 
   <?php
-    print ("You ordered $total_items popcorn items <br />");
-    printf ("Your total bill is: $ %5.2f <br />", $total_price);
+    print ("You ordered $totalProduct popcorn items <br />");
+    printf ("Your total bill is: $ %5.2f <br />", $totalBill);
     print ("Your chosen method of payment is: $payment <br />");
   ?>
 </body>
